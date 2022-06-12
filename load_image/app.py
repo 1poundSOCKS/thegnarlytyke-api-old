32caf5e04b3f7ebab236d184a76b1d9150aa7fa4
1poundSOCKS <mathew.coburn@gmail.com>
2022-06-12 11:44:36 +0100

    Test deploy

--- load_image/app.py
+++ load_image/app.py
@@ -4,13 +4,10 @@ import boto3
 def lambda_handler(event, context):
 
     s3 = boto3.client('s3')
-    print(event)
-    # request_data = event['queryStringParameters']
-    # crag_id = request_data['id']
-    # # data_key = "data/{}.crag.json".format(crag_id)
-    data_key = "610cb7a8-6bd7-4165-ab49-04703a970f6d.txt"
+    request_data = event['queryStringParameters']
+    object_id = request_data['id']
+    data_key = "{}.topo.image.txt".format(object_id)
     obj = s3.get_object(Bucket="images.thegnarlytyke.com", Key=data_key)
-    # j = json.loads(obj['Body'].read().decode('utf-8'))
     t = obj['Body'].read().decode('utf-8')
 
     return {
--- save_image/app.py
+++ save_image/app.py
@@ -9,11 +9,16 @@ def lambda_handler(event, context):
     s3 = boto3.client('s3')
     request_data = event['queryStringParameters']
     object_id = request_data['id']
-    data_key = "{}.txt".format(object_id)
+    data_key = "{}.topo.image.txt".format(object_id)
     s3.put_object(Bucket="images.thegnarlytyke.com", Key=data_key, Body=encoded_string)
 
     return {
         "statusCode": 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
         "body": json.dumps(
             {
                 "message": "save image call successful"
