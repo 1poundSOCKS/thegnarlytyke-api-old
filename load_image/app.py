2566376dd76499a13ba145b666f3764bd8f6136c
1poundSOCKS <mathew.coburn@gmail.com>
2022-06-13 09:17:23 +0100

    Added save crag function

--- load_image/app.py
+++ load_image/app.py
@@ -1,13 +1,15 @@
 import json
 import boto3
 
+BUCKET_NAME = "test.data.thegnarlytyke.com"
+
 def lambda_handler(event, context):
 
     s3 = boto3.client('s3')
     request_data = event['queryStringParameters']
     object_id = request_data['id']
-    data_key = "{}.topo.image.txt".format(object_id)
-    obj = s3.get_object(Bucket="images.thegnarlytyke.com", Key=data_key)
+    data_key = "images/{}.topo.txt".format(object_id)
+    obj = s3.get_object(Bucket=BUCKET_NAME, Key=data_key)
     t = obj['Body'].read().decode('utf-8')
 
     return {
--- save_crag/app.py
+++ save_crag/app.py
@@ -1,7 +1,5 @@
 import json
 import boto3
-import base64
-import datetime
 
 BUCKET_NAME = "test.data.thegnarlytyke.com"
 
@@ -14,19 +12,10 @@ def lambda_handler(event, context):
     request_data = event['queryStringParameters']
     object_id = request_data['id']
 
-    datetime_stamp = datetime.datetime.now().strftime("%G%m%d.%H%M%S.%f")
+    filename = "{}.crag.json".format(object_id,datetime_stamp)
+    data_key = "data/{}".format(filename)
+    s3.put_object(Bucket=BUCKET_NAME, Key=data_key, Body=encoded_string)
     
-    split_pos = string.find(',')
-    base64_image_data = string[split_pos+1:]
-    encoded_data = base64_image_data.encode("utf-8")
-    data_key = "images/{}.{}.topo.txt".format(object_id,datetime_stamp)
-    s3.put_object(Bucket=BUCKET_NAME, Key=data_key, Body=encoded_data)
-    
-    binary_image_data = base64.b64decode(base64_image_data)
-    filename = "{}.{}.topo.jpg".format(object_id,datetime_stamp)
-    data_key = "images/{}".format(filename)
-    s3.put_object(Bucket=BUCKET_NAME, Key=data_key, Body=binary_image_data)
-
     return {
         "statusCode": 200,
         'headers': {
