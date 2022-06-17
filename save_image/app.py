776b4e2ee19f0a361e1f22bb964cab8ffa81af82
1poundSOCKS <mathew.coburn@gmail.com>
2022-06-17 14:33:57 +0100

    Save data function added

--- save_image/app.py
+++ save_image/app.py
@@ -4,6 +4,7 @@ import base64
 import datetime
 
 BUCKET_NAME = "test.data.thegnarlytyke.com"
+STORE_TEXT_DATA = False
 
 def lambda_handler(event, context):
 
@@ -13,17 +14,20 @@ def lambda_handler(event, context):
     s3 = boto3.client('s3')
     request_data = event['queryStringParameters']
     object_id = request_data['id']
+    object_type = request_data['type']
 
     datetime_stamp = datetime.datetime.now().strftime("%G%m%d.%H%M%S.%f")
     
     split_pos = string.find(',')
     base64_image_data = string[split_pos+1:]
-    encoded_data = base64_image_data.encode("utf-8")
-    data_key = "images/{}.{}.topo.txt".format(object_id,datetime_stamp)
-    s3.put_object(Bucket=BUCKET_NAME, Key=data_key, Body=encoded_data)
+    
+    if STORE_TEXT_DATA:
+        encoded_data = base64_image_data.encode("utf-8")
+        data_key = "images/{}.{}.{}.txt".format(object_id,datetime_stamp,object_type)
+        s3.put_object(Bucket=BUCKET_NAME, Key=data_key, Body=encoded_data)
     
     binary_image_data = base64.b64decode(base64_image_data)
-    filename = "{}.{}.topo.jpg".format(object_id,datetime_stamp)
+    filename = "{}.{}.{}.jpg".format(object_id,datetime_stamp,object_type)
     data_key = "images/{}".format(filename)
     s3.put_object(Bucket=BUCKET_NAME, Key=data_key, Body=binary_image_data)
 
